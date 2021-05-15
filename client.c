5f4d2899e0d363cb0367587dc1b43d0b27b87fc2
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2021-05-14 20:59:17 -0600

    ya

--- client.c
+++ client.c
@@ -1,192 +1,78 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
 #include <errno.h>
+#include <string.h>
 #include <pthread.h>
-#include "helper.h"
+#include <sys/types.h>
+#include <signal.h>
 
-#define MAXLINE 1024 /* max line size */
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 2048
+#define NAME_LEN 32
 
-char prompt[]="Chatroom> ";
-int flag=0;
-/*
-get the usage of the script
-*/
-void usage(){
-  printf("-h  print help\n");
-  printf("-a  IP address of the server[Required]\n");
-  printf("-p  port number of the server[Required]\n");
-  printf("-u  enter your username[Required]\n");
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[NAME_LEN];
+
+void str_overwrite_stdout(){
+  printf("\r%s","> " );
+  fflush(stdout);
 }
 
-
-/*
-* @brief-: connects the client to ther sever
-* NOTE-: THE function traverses the list to find appropriate socket Connection [ is robust]
-* @port-: port number
-* @hostname-: ip address of the server
-* @return -: connection file descriptor
-*/
-
-int connection(char* hostname, char* port){
-  int clientfd,rc;
-  struct addrinfo hints, *listp, *p;
-
-  memset(&hints, 0, sizeof(struct addrinfo));
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM; /* Connections only */
-  hints.ai_flags |=AI_ADDRCONFIG;
-  hints.ai_flags |= AI_NUMERICSERV; //using fixed port number
-
-
-  	if ((rc = getaddrinfo(hostname, port, &hints, &listp)) != 0) {
-    	fprintf(stderr,"invalid hostname or port number\n");
-    	return -1;
-	}
-
- 	for (p = listp; p; p = p->ai_next) {
-        /* Create a socket descriptor */
-        clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (clientfd < 0) {
-            continue; /* Socket failed, try the next */
-        }
-
-        /* Connect to the server */
-        if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1) {
-            break; /* Success */
-        }
-
-        /* Connect failed, try another */
-        if (close(clientfd) < 0) {
-            fprintf(stderr, "open_clientfd: close failed: %s\n",
-                    strerror(errno));
-            return -1;
-        }
+void str_trim_lf(char* arr, int lenght){
+  for(int i=0; i<lenght; i++){
+    if(arr[i] == '\n'){
+      arr[i] = '\0';
+      break;
     }
-
-    /* Clean up */
-    freeaddrinfo(listp);
-    if (!p) { /* All connects failed */
-        return -1;
-    }
-    else { /* The last connect succeeded */
-        return clientfd;
-    }
+  }
 }
 
-// read server response
-void reader(void* var){
-  	char buf[MAXLINE];
-  	rio_t rio;
-  	int status;
-  	int connID=(int)var;
-  	// initialise rio data structure
-  	rio_readinitb(&rio, connID);
-  	while(1){
-     	while((status=rio_readlineb(&rio,buf,MAXLINE)) >0){
-          	//error
-          	if(status == -1)
-            	exit(1);
-          	if(!strcmp(buf,"\r\n")){
-              	break;
-            }
-         	// exit from the server
-          	if (!strcmp(buf,"exit")){
-              	close(connID);
-              	exit(0);
-            }
-          	if (!strcmp(buf,"start\n")){
-               	printf("\n");
-            }
-
-          	else
-            	printf("%s",buf);
-        }
-      	// print the Chatroom prompt
-      	printf("%s",prompt);
-      	fflush(stdout);
-    }
+void catch_ctrl_c_and_exit(){
+	flag = 1;
 }
 
 int main(int argc, char **argv){
+  if (argc != 2){
+    printf("Usage: %s <port>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
+  char *ip = "127.0.0.1";
+  int port = atoi(argv[1]);
 
-  	char *address=NULL,*port=NULL,*username=NULL;
-  	char cmd[MAXLINE];
-  	char c;
-  	pthread_t tid;
-  	//parsing command line arguments
-  	while((c = getopt(argc, argv, "hu:a:p:u:")) != EOF){
-    	switch(c){
-    	  	// print help
-    	  	case 'h':
-    	     	usage();
-    	     	exit(1);
-    	     	break;
-    	  	// get server address
-    	  	case 'a':
-    	     	address=optarg;
-    	     	break;
-    	  	// get server port number
-    	  	case 'p':
-    	     	port=optarg;
-    	     	break;
-    	  	// get the username
-    	  	case 'u':
-    	     	username=optarg;
-    	     	break;
+  signal(SIGINT, catch_ctrl_c_and_exit);
 
-    	  	default:
-    	      	usage();
-    	      	exit(1);
+  printf("Enter your name: ");
+  fgets(name, NAME_LEN, stdin);
+  str_trim_lf(name, strlen(name));
 
-    	}
-	}
+  if(strlen(name) > NAME_LEN - 1 || strlen(name) < 2){
+    printf("Enter nsme correctly\n");
+    return EXIT_FAILURE;
+  }
 
-   if(optind  == 1 || port == NULL || address == NULL || username == NULL){
-    printf("Invalid usage\n");
-    usage();
-    exit(1); }
+  struct sosckaddr_in server_addr;
+  //Socket Settings
+  sockfd = socket(AF_INET , SOCK_STREAM, 0);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+  server_addr.sin_port = htons(port);
 
-    int connID=connection(address,port);
-    if(connID == -1){
-       	printf("Couldn't connect to the server\n");
-       	exit(1);
-    }
-    // add a newline
-    sprintf(username,"%s\n",username);
+  //Conect to the server
+  int err = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if(err == -1){
+    printf("ERROR: connect\n");
+    return EXIT_FAILURE;
+  }
 
-    // send the server , your username
-    if(rio_writen(connID,username,strlen(username)) == -1){
-    	perror("not able to send the data");
-    	close(connID);
-    	exit(1);
-    }
+  //Send the name
+  send(sockfd, name, NAME_LEN, 0);
+  printf("=== Bienvenido al CHATROOM====\n");
 
-    // a thread for reading server response
-    pthread_create(&tid,NULL,reader, (void*)connID);
-    // print the Chatroom prompt
-    printf("%s",prompt);
-    while(1){
-      	// read the command
-      	if ((fgets(cmd, MAXLINE, stdin) == NULL) && ferror(stdin)) {
-            perror("fgets error");
-            close(connID);
-            exit(1);
-        }
-
-
-      	// send the request to the server
-      	if (rio_writen(connID,cmd,strlen(cmd)) == -1){
-          	perror("not able to send the data");
-          	close(connID);
-          	exit(1);
-        }
-
-    }
-
+  return EXIT_SUCCESS;
 }
--- server.c
+++ server.c
@@ -1,325 +1,219 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
 #include <errno.h>
+#include <string.h>
 #include <pthread.h>
-#include "helper.h"
+#include <sys/types.h>
+#include <signal.h>
 
-//********************GLOBAL DATA_STRUCTURES & CONSTANTS****************************
-#define bufsize 1000
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 2048
+#define NAME_LEN 32
 
-// mutex lock for global data access
-pthread_mutex_t mutex;
+static _Atomic unsigned int cli_count = 0;
+static int uid = 10;
 
-struct client{
-    char *name;
-    int confd;
-    struct client *next;
-};
+//gcc -Wall -g3 -fsanitize=address -pthread serer.c -o server
+//telnet localhost 4444
 
-struct client *header=NULL;
+//Client Structure - Para poder diferenciar a los clientes
+typedef struct{
+  struct sockaddr_in address;
+  int sockfd;
+  int uid;
+  char name[NAME_LEN];
+}client_t;
 
-//**********************************************************************************
+client_t *clients[MAX_CLIENTS];
 
-/*
- * @brief-: add user to the global user DATA_STRUCTURES
- * INSERTION AN HEAD -> O(1) complexity
-*/
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void add_user(struct client *user){
-
-   if(header == NULL){
-     header=user;
-     user->next=NULL;
-   }
-   else{
-      user->next=header;
-
-      header=user;
-   }
-}
-/*
- * @brief-: delete client from thr global list
- *  O(n) complexity
- */
-void delete_user(confd){
-   struct client *user=header;
-   struct client *previous=NULL;
-   // identify the user
-   while(user->confd!=confd){
-     previous=user;
-     user=user->next;
-   }
-
-   if(previous == NULL)
-      header=user->next;
-
-   else
-     previous->next=user->next;
-
-   // free the resources
-   free(user->name);
-   free(user);
-
+void str_overwrite_stdout(){
+  printf("\r%s","> " );
+  fflush(stdout);
 }
 
-/*
-* @brief-: assigns a listning socket at a given port number
-* NOTE-: THE function traverses the list to find appropriate socket Connection
-* for the server [ isrobust]
-* @port-: port number
-* @return -: listining file descriptor
-*/
-int connection(char * port){
-
-   struct addrinfo *p, *listp, hints;
-   int rc,listenfd,optval=1;
-
-   //initialise to zero
-   memset(&hints,0,sizeof(struct addrinfo));
-   hints.ai_family = AF_INET;
-   hints.ai_socktype = SOCK_STREAM; /* Connections only */
-   hints.ai_flags =AI_ADDRCONFIG|AI_PASSIVE;
-   hints.ai_flags |= AI_NUMERICSERV; //using fixed port number
-
-
-   if ((rc = getaddrinfo(NULL, port, &hints, &listp)) != 0) {
-     fprintf(stderr,"get_address info failed port number %s is invalid\n",port);
-     return -1;
+void str_trim_lf(char* arr, int lenght){
+  for(int i=0; i<lenght; i++){
+    if(arr[i] == '\n'){
+      arr[i] = '\0';
+      break;
+    }
   }
+}
 
-   // traverse the list of available Connections
-   for (p = listp; p; p = p->ai_next) {
+void queue_add(client_t *cl){
+  pthread_mutex_lock(&clients_mutex);
 
-       listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-       if (listenfd < 0) {
-         continue; /* Socket failed, try the next */
-       }
+  for(int i=0; i<MAX_CLIENTS; i++){
+    if(!clients[i]){
+      clients[i] = cl;
+      break;
+    }
+  }
+  pthread_mutex_unlock(&clients_mutex);
+}
 
-       /* Eliminates "Address already in use" error from bind */
-      setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,sizeof(int));
-      //bind the socket, returns 0 on Success
-      if (bind(listenfd, p->ai_addr, p->ai_addrlen) == 0) {
-          break; /* Success */
+void queue_remove(int uid){
+  pthread_mutex_lock(&clients_mutex);
+
+  for(int i=0; i<MAX_CLIENTS; i++){
+    if(clients[i]){
+      if(clients[i] -> uid == uid){
+        clients[i] = NULL;
+        break;
       }
-      if (close(listenfd) < 0) { /* Bind failed, try the next */
-            fprintf(stderr, "open_listenfd close failed: %s\n",
-                    strerror(errno));
-            return -1;
-        }
-
     }
-
-    // avoiding memory leak
-    freeaddrinfo(listp);
-    if (!p) { /* All connects failed */
-        return -1;
-    }
-
-    // setting backlog to 1024 , desired value
-    // set the socket to listen
-    if (listen(listenfd, 1024) < 0) {
-            close(listenfd);
-            return -1;
-        }
-
-      return listenfd;
+  }
+  pthread_mutex_unlock(&clients_mutex);
 }
 
-/*
- * send msg to all the clients
- */
-void send_msg(int confd,char* msg, char* receiver, char* sender){
 
-    char response[bufsize];
-    struct client *user=header;
-    if(receiver == NULL)
-     while (user != NULL){
-      if (user->confd == confd){
-         strcpy(response,"msg sent\n\r\n");
-         rio_writen(user->confd,response,strlen(response));
-       }
+void print_ip_addr(struct sockaddr_in addr) {
+  printf("%d.%d.%d.%d",
+  addr.sin_addr.s_addr & 0xff,
+  (addr.sin_addr.s_addr & 0xff00) >> 8,
+  (addr.sin_addr.s_addr & 0xff0000) >> 16,
+  (addr.sin_addr.s_addr & 0xff000000) >> 24);
 
-      else{
-         sprintf(response,"start\n%s:%s\n\r\n",sender,msg);
-         rio_writen(user->confd,response,strlen(response));
+}void send_message(char *s, int uid){
+  pthread_mutex_lock(&clients_mutex);
+
+  for(int i=0;i<MAX_CLIENTS;i++){
+      if(clients[i]){
+        if (clients[i]->uid != uid){
+          if(write(clients[i]-> sockfd,s,strlen(s))<0){
+            printf("ERROR: write to descriptor failed\n");
+            break;
+          }
+        }
       }
-      user=user->next;
-    }
-   else{
-       while (user != NULL){
-         if(!strcmp(user->name,receiver)){
-           sprintf(response,"start\n%s:%s\n\r\n",sender,msg);
-           rio_writen(user->confd,response,strlen(response));
-           strcpy(response,"msg sent\n\r\n");
-           rio_writen(confd,response,strlen(response));
-           return;
-         }
-         user=user->next;
-       }
-        strcpy(response,"user not found\n\r\n");
-        rio_writen(confd,response,strlen(response));
-
-   }
-
+  }
+  pthread_mutex_unlock(&clients_mutex);
 }
 
-void evaluate(char *buf ,int confd ,char *username){
+void *handle_client(void *arg){
+  char buff_out[BUFFER_SZ];
+  char name[NAME_LEN];
+  int leave_flag = 0; //para ver si hay algun error y desconectar
+  cli_count++;
 
-  char response[bufsize];
-  char msg[bufsize];
-  char receiver[bufsize];
-  char keyword[bufsize];
-  // clear the buffer
-  msg[0]='\0';
-  receiver[0]='\0';
-  keyword[0]='\0';
-  struct client *user=header;
+  client_t *cli = (client_t*)arg;
 
-
-  if(!strcmp(buf,"help")){
-    sprintf(response,"msg \"text\" : send the msg to all the clients online\n");
-    sprintf(response,"%smsg \"text\" user :send the msg to a particular client\n",response);
-    sprintf(response,"%sonline : get the username of all the clients online\n",response);
-    sprintf(response,"%squit : exit the chatroom\n\r\n",response);
-    rio_writen(confd,response,strlen(response));
-    return;
+  //nombre del clientes
+  if(recv(cli -> sockfd, name, NAME_LEN, 0) <= 0 || strlen(name)< 2|| strlen(name)>= NAME_LEN-1){
+    printf("Ingrese el nombre correctamente\n");
+    leave_flag = 1;
+  } else{
+    strcpy(cli -> name, name);
+    sprintf(buff_out, "%s se ha unido\n", cli->name);
+    printf("%s",buff_out);
+    send_message(buff_out, cli-> uid);
   }
-   // get the online user name
-  if (!strcmp(buf,"online")){
-    // empty the buffer
-    response[0]='\0';
-    //global access should be exclusive
-    pthread_mutex_lock(&mutex);
-    while(user!=NULL){
-      sprintf(response,"%s%s\n",response,user->name);
-      user=user->next;
+  bzero(buff_out, BUFFER_SZ);
+
+  while(1){
+    if(leave_flag){
+      break;
     }
-    sprintf(response,"%s\r\n",response);
-    //global access should be exclusive
-    pthread_mutex_unlock(&mutex);
-    rio_writen(confd,response,strlen(response));
-    return;
-  }
+    int receive = recv(cli->sockfd, buff_out, BUFFER_SZ, 0);
 
-  if (!strcmp(buf,"quit")){
-    pthread_mutex_lock(&mutex);
-    delete_user(confd);
-    pthread_mutex_unlock(&mutex);
-    strcpy(response,"exit");
-    rio_writen(confd,response,strlen(response));
-    close(confd);
-    return;
-  }
-
-   sscanf(buf,"%s \" %[^\"] \"%s",keyword,msg,receiver);
-
-    if (!strcmp(keyword,"msg")){
-      pthread_mutex_lock(&mutex);
-      send_msg(confd,msg,receiver,username);
-      pthread_mutex_unlock(&mutex);
+    if(receive > 0){
+      if(strlen(buff_out)>0){
+        send_message(buff_out, cli-> uid);
+        str_trim_lf(buff_out, strlen(buff_out));
+        printf("%s -> %s", buff_out, cli-> name);
+      }
+    } else if(receive ==0 || strcmp(buff_out, "exit")==0){
+      sprintf(buff_out, "%s se salio\n", cli-> name);
+      printf("%s",buff_out);
+      send_message(buff_out, cli-> uid);
+      leave_flag = 1;
+    }else{
+      printf("ERROR: -1\n");
+      leave_flag = 1;
     }
-  else {
-    strcpy(response,"Invalid command\n\r\n");
-    rio_writen(confd,response,strlen(response));
+    bzero(buff_out, BUFFER_SZ);
   }
-
-}
-/*
-* @brief-: the function handles incoming clients concurrently
-* @vargp-: poiner to the connection file descriptor
-*/
-void* client_handler(void *vargp ){
-
-  char username[bufsize];
-  rio_t rio;
-  struct client *user;
-  long byte_size;
-  char buf[bufsize];
-  // detaching the thread from peers
-  // so it no longer needs to be
-  // terminated in the main thread
+  close(cli-> sockfd);
+  queue_remove(cli-> uid);
+  free(cli);
+  cli_count--;
   pthread_detach(pthread_self());
 
-  // saving the connection fd on function stack
-  int confd = *((int *)vargp);
-  rio_readinitb(&rio, confd);
-
-  // read the user name as a single line , -1 is for error handling
-  if( (byte_size=rio_readlineb(&rio,username,bufsize)) == -1){
-    close(confd);
-    free(vargp);
-    return NULL;
-  }
-  //strip the newline from the string
-  username[byte_size-1]='\0';
-  // assign space in the global structure
-  user=malloc(sizeof(struct client));
-  // error handling
-  if (user == NULL){
-    perror("memory can't be assigned");
-    close(confd);
-    free(vargp);
-    return NULL;
-  }
-
-  // user->name=username is not safe
-  // as the local stack can be accessed by peer threads
-  // assign space in heap
-  user->name=malloc(sizeof(username));
-  memcpy(user->name,username,strlen(username)+1);
-  user->confd=confd;
-  //lock
-  pthread_mutex_lock(&mutex);
-  add_user(user);
-  //unlock
-  pthread_mutex_unlock(&mutex);
-  // read client response
-  while((byte_size=rio_readlineb(&rio,buf,bufsize)) >0){
-    //strip the newline from the string
-    buf[byte_size-1]='\0';
-    // take appropriate action
-    evaluate(buf,confd,username);
-  }
   return NULL;
 }
 
-int main(int argc,char **argv){
-  struct sockaddr_storage clientaddr;
-  socklen_t clientlen;
-  int listen=-1;
-  char host[1000];
-  char *port="80";
-  int *confd;
+int main(int argc, char **argv){
+  if (argc != 2){
+    printf("Usage: %s <port>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  char *ip = "192.168.1.6";
+  int port = atoi(argv[1]);
+
+  int option = 1;
+  int listenfd = 0, connfd = 0;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in cli_addr;
   pthread_t tid;
 
-  if (argc > 1)
-    port=argv[1];
+  //Socket Settings
+  listenfd = socket(AF_INET , SOCK_STREAM, 0);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(ip);
+  serv_addr.sin_port = htons(port);
 
-  // make a connection file descriptor
-  listen= connection(port);
 
-  //connection failed
-  if(listen == -1){
-    printf("connection failed\n");
-    exit(1);
+  //Signals - software generated interrupts
+  signal(SIGPIPE, SIG_IGN);
+
+  if(setsockopt(listenfd, SOL_SOCKET, (SO_REUSEPORT, SO_REUSEADDR), (char*)&option, sizeof(option))<0){
+    printf("ERROR: setsockopt\n");
+    return EXIT_FAILURE;
   }
 
-  printf("waiting at localhost and port '%s' \n",port);
+  //Bind
+  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0){
+    printf("ERROR: bind\n");
+    return EXIT_FAILURE;
+  }
 
-  // loop to keep accepting clients
+  //listen
+  if(listen(listenfd,10)<0){
+    printf("ERROR: listen\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("=== BIENVENIDO A EL CHATROOM DE BLOCK Y KRISTEN ===\n");
+
   while(1){
-    // assign space in the heap [prevents data race]
-    confd=malloc(sizeof(int));
-    *confd=accept(listen, (struct sockaddr *)&clientaddr, &clientlen);
-    printf("A new client is online\n");
-    // assign a seperate thread to deal with the new client
-    pthread_create(&tid,NULL,client_handler, confd);
+    socklen_t clilen = sizeof(cli_addr);
+    connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
 
+    //Check for MAX Client
+    if((cli_count +1 ) == MAX_CLIENTS){
+      printf("Maximo numero de clientes conectados. Conexion no valida\n" );
+      print_ip_addr(cli_addr);
+      close(connfd);
+      continue;
+    }
+    //client Settings
+    client_t *cli = (client_t *)malloc(sizeof(client_t));
+    cli -> address = cli_addr;
+    cli -> sockfd = connfd;
+    cli -> uid = uid ++;
+
+    //add client to queue
+    queue_add(cli);
+    pthread_create(&tid, NULL, &handle_client, (void*)cli);
+
+    //reduce CPU Usage
+    sleep(1);
   }
-
+  return EXIT_SUCCESS;
 }
